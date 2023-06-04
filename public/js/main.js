33c1d3cf6768904486c1a3e6976711db4b7d4420
adityasingh-fun <chaudharyaditya41@gmail.com>
2023-06-05 04:14:53 +0530

    Second commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,48 @@
-const socket = io(); // we have access to io because of script tag that we added in chat.html 
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
+// Get username and room from URL
+const {username,room} = Qs.parse(location.search,{
+    ignoreQueryPrefix: true
+});
+console.log(username,room)
+
+const socket = io(); // we have access to io because of script tag that we added in chat.html
+
+socket.emit('joinRoom',{username,room})
+
+socket.on('message',message => {
+    console.log(message);
+    outputMessage(message);
+
+    // Scroll Down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+})
+
+// We are creating event listener for submission of form line 1
+// Message submit
+chatForm.addEventListener('submit',(e)=>{
+    // we are passing event in the function here because when you submit somethig, it gets stored in a file and we need to prevent that
+    e.preventDefault(); // This will prevent the default behavior of storing the form to a file 
+
+    // Get message text from the form
+    const msg = e.target.elements.msg.value;  
+    
+    // Emit message to server and server will actch the message and wthen will emit back to client
+    socket.emit('chatMessage',msg)
+
+    // Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+    
+})
+
+function outputMessage(message){
+    const div = document.createElement("div");
+    div.classList.add('message');
+    div .innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+      ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div)
+}
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path');    // Node JS core module
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io')
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
 
 const app = express();
 // Method create server is used by express but we want to access directly in order to use socket.io
@@ -11,10 +12,33 @@ const io = socketio(server);
 // Set static folder(we will set the public folder as static folder so that we can access our frotend)
 app.use(express.static(path.join(__dirname,'public')));
 
+const botName = 'ChatCord Bot';
+
 // Run when client connects
 io.on('connection',socket =>{
     console.log('New website connection');
+
+    // Emits message to the client 
+    // socket.emit emits to single client that is connected
+    socket.emit('message',formatMessage(botName,'Welcome to chatCord'));     // we will catch this message in main.js 
+
+    // Broadcast when a user connects
+    // socket.broadcast.emit will emit to everyone except the connected user
+    socket.broadcast.emit('message',formatMessage(botName, 'A user has joined the chat'));
+    
+    // Runs when client disconnects
+    socket.on('disconnect',()=>{
+        // io.emit emits to every single user
+        io.emit('message',formatMessage(botName,'A user has left the chat'));
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg)=>{
+        console.log(msg);
+        io.emit('message',formatMessage('User',msg))
+    })
 });
+
 
 server.listen(process.env.PORT || 3000, function(){
     console.log('Express app running at port ' + (process.env.PORT || 3000))
